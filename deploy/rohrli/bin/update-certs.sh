0da9ef2b6b050d5c7a49263b987b84336ce460dc
Roman Stoffel <roman.stoffel@gamlor.info>
2017-06-16 17:58:04 +0900

    Fix SSL key rotation.

--- deploy/rohrli/bin/update-certs.sh
+++ deploy/rohrli/bin/update-certs.sh
@@ -2,7 +2,7 @@
 
 SOURCE=/opt/local/etc/letsencrypt/live/rohrli.gamlor.info
 TARGET=/etc/rohrli
-openssl pkcs12 -inkey $SOURCE/privkey.pem -in $SOURCE/fullchain.pem -export -out $TARGET/cert-key.pkcs12
+openssl pkcs12 -inkey $SOURCE/privkey.pem -in $SOURCE/fullchain.pem -export -passout pass: -out $TARGET/cert-key.pkcs12
 chown -R rohrlid:rohrlid $TARGET/cert-key.pkcs12
 svcadm restart rohrlid
 
