acbedb6c58cedef108a960440a5cbfac29337d0e
Roman Stoffel <roman.stoffel@gamlor.info>
2017-07-14 21:54:07 +0200

    Fix cert renewal scripts

--- deploy/rohrli/bin/update-certs.sh
+++ deploy/rohrli/bin/update-certs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-
+svcadm disable rohrlid
 SOURCE=/opt/local/etc/letsencrypt/live/rohrli.gamlor.info
 TARGET=/etc/rohrli
 openssl pkcs12 -inkey $SOURCE/privkey.pem -in $SOURCE/fullchain.pem -export -passout pass: -out $TARGET/cert-key.pkcs12
 chown -R rohrlid:rohrlid $TARGET/cert-key.pkcs12
-svcadm restart rohrlid
+svcadm enable rohrlid
 
 echo "certificate copied and server restarted"
