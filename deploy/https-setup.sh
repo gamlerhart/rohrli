43822d1b45b0e06702cf7f0a7b2282b6dcf9481b
Roman Stoffel <roman.stoffel@gamlor.info>
2018-05-16 17:11:41 +0900

    https setup: comment and fixes

--- deploy/https-setup.sh
+++ deploy/https-setup.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-
+# Manual setup steps right now.
 pkgin -y install tmux
 pkgin -y install py27-certbot
 
 # Getting certs
 certbot --standalone -d rohrli.gamlor.info certonly
 #
-certbot renew --dry-run
+
+certbot renew --pre-hook "/etc/rohrli/bin/preupdate-certs.sh" --post-hook "/etc/rohrli/bin/update-certs.sh"
+# Crontab -e
+42 6 * * * certbot renew --pre-hook "/etc/rohrli/bin/preupdate-certs.sh" --post-hook "/etc/rohrli/bin/update-certs.sh"
